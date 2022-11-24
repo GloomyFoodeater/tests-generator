0ca533b22c45b7058eab44254e6b95f890accb95
Siarhei Paulavets <s.paulavets@gmail.com>
2022-11-24 22:11:20 +0300

    Fix namespace and using directives
    Put tests namespace in the outermost namespace of tested class instead of innermost.
    Generate using directives from all outer namespaces of source class instead of only declared, i.e. if source class was inside of file scoped namespace Foo.Bar both Foo and Foo.Bar will be used.

--- TestsGenerator.Core/Generation/XUnitGenerator.cs
+++ TestsGenerator.Core/Generation/XUnitGenerator.cs
@@ -11,39 +11,39 @@ public class XUnitGenerator : ITestGenerator
     public IEnumerable<TestsInfo> Generate(string source)
     {
         // Extract members from source unit.
-        var sourceUnit = CSharpSyntaxTree.ParseText(source).GetCompilationUnitRoot();
+        var unit = CSharpSyntaxTree.ParseText(source).GetCompilationUnitRoot();
 
-        var hasErrors = sourceUnit
+        var hasErrors = unit
             .GetDiagnostics()
             .Any(diagnostic => diagnostic.Severity == DiagnosticSeverity.Error);
         if (hasErrors)
             throw new SourceSyntaxException($"Source unit has syntax errors");
 
-        var sourceClasses = sourceUnit
+        var classes = unit
             .DescendantNodes()
             .OfType<ClassDeclarationSyntax>()
             .Where(@class => @class.Modifiers.Any(SyntaxKind.PublicKeyword)) // Only public classes
             .ToArray(); // Multiple enumerations below => immediate execution
 
-        var sourceUsingDirectives = sourceUnit
+        var usingDirectives = unit
             .DescendantNodes()
             .OfType<UsingDirectiveSyntax>()
             .Where(usingDirective => usingDirective.StaticKeyword.IsMissing);
 
-        if (!sourceClasses.Any())
+        if (!classes.Any())
             throw new SourceSyntaxException("Source unit does not contain any classes");
 
         // Generate sequentially methods, classes, namespaces, using directives and units
-        // for each source class and append new test created with them in result collection.
-        return from sourceClass in sourceClasses
-            let sourceNamespaceName = ExtractSourceNamespaceFullName(sourceClass)
-            let testsMethods = GenerateTestsMethods(sourceClass, _ => GetFailedTestBlock())
-            let testsClass = GenerateTestsClass(testsMethods, sourceClass)
-            let testsNamespace = GenerateTestsNamespace(testsClass, sourceNamespaceName)
-            let testsUsingDirectives = GenerateTestsUsingDirectives(sourceUsingDirectives, sourceNamespaceName)
+        // for each source class and create tests from units.
+        return from @class in classes
+            let namespacesNames = GetFullNamespacesNamesFrom(@class)
+            let testsMethods = GenerateTestsMethods(@class, _ => GetFailedTestBlock())
+            let testsClass = GenerateTestsClass(testsMethods, @class)
+            let testsNamespace = GenerateTestsNamespace(testsClass, namespacesNames)
+            let testsUsingDirectives = GenerateTestsUsingDirectives(usingDirectives, namespacesNames)
             let testsUnit = GenerateTestsUnit(testsUsingDirectives, testsNamespace)
             select new TestsInfo(
-                sourceClass.Identifier.Text,
+                @class.Identifier.Text,
                 testsUnit.NormalizeWhitespace().ToFullString());
     }
 }
