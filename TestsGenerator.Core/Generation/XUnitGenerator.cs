0d0d1e475302b2b82b348fda19500033e41502ea
Siarhei Paulavets <s.paulavets@gmail.com>
2022-11-24 13:53:04 +0300

    Fix overloaded methods & clean up

--- TestsGenerator.Core/Generation/XUnitGenerator.cs
+++ TestsGenerator.Core/Generation/XUnitGenerator.cs
@@ -10,45 +10,31 @@ public class XUnitGenerator : ITestGenerator
 {
     public IEnumerable<TestsInfo> Generate(string source)
     {
-        var tree = CSharpSyntaxTree.ParseText(source);
-        var sourceUnit = tree.GetCompilationUnitRoot();
+        // Extract members from source unit.
+        var sourceUnit = CSharpSyntaxTree.ParseText(source).GetCompilationUnitRoot();
         var sourceClasses = sourceUnit
             .DescendantNodes()
             .OfType<ClassDeclarationSyntax>()
-            .Where(@class => @class.Modifiers.Any(SyntaxKind.PublicKeyword))
+            .Where(@class => @class.Modifiers.Any(SyntaxKind.PublicKeyword)) // Only public classes
             .ToArray();
         var sourceUsingDirectives = sourceUnit
             .DescendantNodes()
-            .OfType<UsingDirectiveSyntax>()
-            .ToArray();
-
+            .OfType<UsingDirectiveSyntax>();
+        
         if (!sourceClasses.Any())
             throw new ClassCountException("Source does not contain any classes");
 
-        var testsInfos = new List<TestsInfo>();
-        foreach (var sourceClass in sourceClasses)
-        {
-            // Generate methods
-            var testsMethods = GenerateTestsMethods(sourceClass);
-
-            // Generate class
-            var testsClass = GenerateTestsClass(testsMethods, sourceClass);
-
-            // Generate namespace
-            var sourceNamespaceName = ExtractSourceNamespaceFullName(sourceClass);
-            var testsNamespace = GenerateTestsNamespace(testsClass, sourceNamespaceName);
-
-            // Generate use directives
-            var testsUsingDirectives = GenerateTestsUsingDirectives(sourceUsingDirectives, sourceNamespaceName);
-
-            // Generate unit
-            var testsUnit = GenerateTestsUnit(testsUsingDirectives, testsNamespace);
-
-            var testsName = sourceClass.Identifier.Text + "Tests";
-            var testsContent = testsUnit.NormalizeWhitespace().ToFullString();
-            testsInfos.Add(new(testsName, testsContent));
-        }
-
-        return testsInfos;
+        // Generate sequentially methods, classes, namespaces, using directives and units
+        // for each source class and append new test created with them in result collection.
+        return from sourceClass in sourceClasses
+            let sourceNamespaceName = ExtractSourceNamespaceFullName(sourceClass)
+            let testsMethods = GenerateTestsMethods(sourceClass, _ => GetFailedTestBlock())
+            let testsClass = GenerateTestsClass(testsMethods, sourceClass)
+            let testsNamespace = GenerateTestsNamespace(testsClass, sourceNamespaceName)
+            let testsUsingDirectives = GenerateTestsUsingDirectives(sourceUsingDirectives, sourceNamespaceName)
+            let testsUnit = GenerateTestsUnit(testsUsingDirectives, testsNamespace)
+            select new TestsInfo(
+                sourceClass.Identifier.Text + "Tests",
+                testsUnit.NormalizeWhitespace().ToFullString());
     }
 }
