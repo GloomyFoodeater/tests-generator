98ee6e326acccd535680b73de2525d5ac6cb3562
Siarhei Paulavets <s.paulavets@gmail.com>
2022-11-25 20:33:27 +0300

    Add pipeline for files

---
+++ TestsGenerator.Core/Dataflow/Pipeline.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.Tasks.Dataflow;
+using TestsGenerator.Core.Generation;
+
+namespace TestsGenerator.Core.Dataflow;
+
+public class Pipeline
+{
+    private readonly PipelineConfiguration _configuration;
+
+    public Pipeline(PipelineConfiguration configuration) => _configuration = configuration;
+
+    public async Task PerformProcessing(IEnumerable<string> filePaths)
+    {
+        // Create pipeline blocks.
+        var readingBlock = new TransformBlock<string, string>(
+            ReadContent,
+            new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = _configuration.MaxReadingTasks });
+
+        var processingBlock = new TransformManyBlock<string, TestsInfo>(
+            GenerateTests,
+            new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = _configuration.MaxProcessingTasks });
+
+        var writingBlock = new ActionBlock<TestsInfo>(
+            WriteTests,
+            new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = _configuration.MaxWritingTasks });
+
+        // Connect pipeline blocks into one network.
+        var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+        readingBlock.LinkTo(processingBlock, linkOptions);
+        processingBlock.LinkTo(writingBlock, linkOptions);
+
+        // Post files into network queue.
+        foreach (var filePath in filePaths)
+            readingBlock.Post(filePath);
+
+        // Wait last block completion.
+        readingBlock.Complete();
+        await writingBlock.Completion;
+    }
+
+    private static async Task<string> ReadContent(string filePath)
+    {
+        using var streamReader = new StreamReader(filePath);
+        var result = await streamReader.ReadToEndAsync();
+        return result;
+    }
+
+    private IEnumerable<TestsInfo> GenerateTests(string fileContent)
+    {
+        var generator = new XUnitGenerator();
+        return _configuration.TestsGenerator // Generator must be thread safe
+            .Generate(fileContent)
+            .ToArray(); // Immediate execution
+    }
+
+    private async Task WriteTests(TestsInfo testsInfo)
+    {
+        var testsPath = $"{_configuration.SavePath}\\{testsInfo.Name}Tests.cs";
+        await using var streamWriter = new StreamWriter(testsPath);
+        await streamWriter.WriteAsync(testsInfo.Content);
+    }
+}
---
+++ TestsGenerator.Core/Dataflow/PipelineConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using TestsGenerator.Core.Generation;
+
+namespace TestsGenerator.Core.Dataflow;
+
+public record PipelineConfiguration
+{
+    private const int DefaultReadingTasks = 5;
+    private const int DefaultProcessingTasks = 5;
+    private const int DefaultWritingTasks = 5;
+    private const string DefaultSavePath = ".";
+    private static readonly XUnitGenerator DefaultGenerator = new(); // XUnitGenerator is immutable
+
+    public int MaxReadingTasks { get; init; } = DefaultReadingTasks;
+    public int MaxProcessingTasks { get; init; } = DefaultProcessingTasks;
+    public int MaxWritingTasks { get; init; } = DefaultWritingTasks;
+    public string SavePath { get; init; } = DefaultSavePath;
+    public ITestGenerator TestsGenerator { get; init; } = DefaultGenerator;
+}
